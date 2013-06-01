fc7ae4b072b35c3c460a2a927e12bd9b58cb53b3
João <joaogabrielv@gmail.com>
2013-05-31 23:11:13 -0300

    Protótipo - Home: Artigos e Depoimentos

--- wp-content/themes/foccus/index.php
+++ wp-content/themes/foccus/index.php
@@ -1,35 +1,131 @@
 <? get_template_part('incs/head') ?>
 
-  <? get_template_part('incs/topo') ?>
+<? get_template_part('incs/topo') ?>
 
-  <div class="conteudo">
+<div class="conteudo">
 
-    <div class="slideshow">
+  <div class="slideshow">
 
-    </div>   
+  </div>   
 
-    <div class="newsletter">
-      <span>Receba nossa newsletter</span>
-      <form action="" method="post" class="float-right">
-        <label for="news_nome">Nome</label>
-        <input type="text" name="news_nome" id="news_nome" />
-        <label for="news_email">email</label>
-        <input type="text" name="news_email" id="news_email" />
-        <input type="submit" value="enviar" />
-      </form>
-    </div>
-
-    <div class="ultimas-noticias">
-      <span class="nome-secao">Últimas Notícias</span>
-      <span class="linha-azul"></span>
-      
-      <div class="box">
-        
-      </div>
-      
-    </div>
-    
-    
+  <div class="newsletter">
+    <span>Receba nossa newsletter</span>
+    <form action="" method="post" class="float-right">
+      <label for="news_nome">Nome</label>
+      <input type="text" name="news_nome" id="news_nome" />
+      <label for="news_email">email</label>
+      <input type="text" name="news_email" id="news_email" />
+      <input type="submit" value="enviar" />
+    </form>
   </div>
 
-  <? get_template_part('incs/footer') ?>
+  <div class="linha-boxes">
+    
+    <span class="nome-secao">Últimas Notícias</span>
+    <span class="linha-azul"></span>
+
+    <div class="box mg-box-right">
+      <a href="#">
+        <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" />
+        <h2 class="fonte-textos">Foccus abre nova unidade no Leblon - Confira</h2>
+      </a>
+    </div>
+    
+    <div class="box mg-box-right">
+      <a href="#">
+        <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" />
+        <h2 class="fonte-textos" >Foccus abre nova unidade no Leblon - Confira</h2 class="fonte-textos" >
+      </a>
+    </div>
+    
+    <div class="box mg-box-right">
+      <a href="#">
+        <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" />
+        <h2 class="fonte-textos" >Foccus abre nova unidade no Leblon - Confira</h2 class="fonte-textos" >
+      </a>
+    </div>
+    
+    <div class="no-mg-right box ">
+      <a href="#">
+        <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" />
+        <h2 class="fonte-textos" >Foccus abre nova unidade no Leblon - Confira</h2 class="fonte-textos" >
+      </a>
+    </div>    
+
+  </div>
+  
+  
+  <div class="linha-boxes">
+    
+    <span class="nome-secao">Artigos</span>
+    <span class="linha-azul"></span>
+
+    <div class="box mg-box-right">
+      <a href="#">
+        <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" />
+        <h2 class="fonte-textos" >Foccus abre nova unidade no Leblon - Confira</h2 class="fonte-textos" >
+      </a>
+    </div>
+    
+    <div class="box mg-box-right">
+      <a href="#">
+        <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" />
+        <h2 class="fonte-textos" >Foccus abre nova unidade no Leblon - Confira</h2 class="fonte-textos" >
+      </a>
+    </div>
+    
+    <div class="box mg-box-right">
+      <a href="#">
+        <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" />
+        <h2 class="fonte-textos" >Foccus abre nova unidade no Leblon - Confira</h2 class="fonte-textos" >
+      </a>
+    </div>
+    
+    <div class="no-mg-right box ">
+      <a href="#">
+        <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" />
+        <h2 class="fonte-textos" >Foccus abre nova unidade no Leblon - Confira</h2 class="fonte-textos" >
+      </a>
+    </div>    
+
+  </div>  
+
+  <div class="linha-boxes">
+    
+    <span class="nome-secao">Depoimentos</span>
+    <span class="linha-azul"></span>
+
+    <div class="box mg-box-right">
+      <a href="#">
+        <h3>Letícia Freitas de Souza</h3>
+        <p class="fonte-textos">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sit amet, consectetuer adipiscing elit, </p>
+      </a>
+    </div>
+    
+    <div class="box mg-box-right">
+      <a href="#">
+        <h3>Letícia Freitas de Souza</h3>
+        <p class="fonte-textos">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sit amet, consectetuer adipiscing elit, </p>
+      </a>
+    </div>
+    
+    <div class="box mg-box-right">
+      <a href="#">
+        <h3>Letícia Freitas de Souza</h3>
+        <p class="fonte-textos">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sit amet, consectetuer adipiscing elit, </p>
+      </a>
+    </div>
+    
+    <div class="box no-mg-right">
+      <a href="#">
+        <h3>Letícia Freitas de Souza</h3>
+        <p class="fonte-textos">Lorem ipsum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sit amet, consectetuer adipiscing elit,Lorem ipsum dolor sit amet, consectetuer adipiscing elit, </p>
+      </a>
+    </div>    
+
+  </div>    
+  
+
+</div>
+
+<? get_template_part('incs/footer') ?>
