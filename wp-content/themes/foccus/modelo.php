417192b7244831f6713de64853875be3e3f59b4e
João <joaogabrielv@gmail.com>
2013-06-18 22:23:30 -0300

    Contato: 60%

--- wp-content/themes/foccus/modelo.php
+++ wp-content/themes/foccus/modelo.php
@@ -1,7 +1,18 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+<?
+get_template_part('incs/head');
+get_template_part('incs/topo');
 ?>
+
+<div class="conteudo">
+
+  <img src="<?= get_bloginfo('template_url') ?>/imgs/arco.png" class="arco">
+  <span class="nome-secao">Título da Página</span>
+  <span class="linha-azul"></span>
+
+
+  <div class="clear"></div>
+
+</div>
+
+
+<? get_template_part('incs/rodape') ?>
