b341c20051b438d2c5eabedeed03f33e1b2837fd
João <joaogabrielv@gmail.com>
2013-06-23 16:40:35 -0300

    integrando barra lateral

--- wp-content/themes/foccus/incs/barra-lateral.php
+++ wp-content/themes/foccus/incs/barra-lateral.php
@@ -2,7 +2,7 @@
 <div class="coluna-direita">
 
   <?
-  if (is_single()) {
+  if (is_single() && false) {
     ?>
 
     <div class="secao">
@@ -37,13 +37,11 @@
 
   <?
   $wp_query = new WP_Query(array('cat' => _DEPOIMENTOS, 'tag' => 'destaque', 'posts_per_page' => 2));
-  //echo '<pre>'; var_dump($wp_query); echo '</pre>';
-  $i = 0;
   while (have_posts()) {
     the_post();
     ?>
     <div class="box">
-      <a href="<?  bloginfo('url')?>/depoimentos/#<? the_ID() ?>">
+      <a href="<? bloginfo('url') ?>/depoimentos/#<? the_ID() ?>">
         <h4 class="fonte-textos">"<?= substr(get_the_excerpt(), 0, 120) ?>..."</h4>        
       </a>
     </div>
@@ -57,23 +55,22 @@
     Notícias
   </div>
 
-  <div class="box">
-    <a href="#">
-      <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" class="float-left" />
-      <h4 class="fonte-textos">san sapien, nec pulvinar mauris vestibulum blandit. Nullam congue felis in tellus vulputate 
-        id lobortis.</h4>
-      <div class="saiba-mais fonte-textos">Saiba +</div>
-    </a>
-  </div>
+  <?
+  $wp_query = new WP_Query(array('cat' => _NOTICIAS, 'posts_per_page' => 2));
+  while (have_posts()) {
+    the_post();
+    ?>  
 
-  <div class="box">
-    <a href="#">
-      <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" class="float-left" />
-      <h4 class="fonte-textos">san sapien, nec pulvinar mauris vestibulum blandit. Nullam congue felis in tellus vulputate 
-        id lobortis.</h4>
-      <div class="saiba-mais fonte-textos">Saiba +</div>
-    </a>
-  </div>    
+    <div class="box">
+      <a href="<? the_permalink() ?>">
+        <? if (has_post_thumbnail()) the_post_thumbnail('post-thumbnail', array('class' => 'float-left')) ?>
+        <h4 class="fonte-textos"><? the_title() ?></h4>
+        <div class="saiba-mais fonte-textos">Saiba +</div>
+      </a>
+    </div>
+    <?
+  }
+  ?>
 
   <div class="clear"></div>
 
@@ -81,23 +78,23 @@
     Artigos
   </div>
 
+  <?
+  $wp_query = new WP_Query(array('cat' => _ARTIGOS, 'tag' => 'destaque'));
+  
+  while (have_posts()) {
+    the_post();
+    ?>  
+  
   <div class="box">
-    <a href="#">
-      <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" class="float-left" />
-      <h4 class="fonte-textos">san sapien, nec pulvinar mauris vestibulum blandit. Nullam congue felis in tellus vulputate 
-        id lobortis.</h4>
+    <a href="<?the_permalink()?>">
+      <? if (has_post_thumbnail()) the_post_thumbnail('post-thumbnail', array('class' => 'float-left')) ?>
+      <h4 class="fonte-textos"><?the_title()?></h4>
       <div class="saiba-mais fonte-textos">Saiba +</div>
     </a>
   </div>
 
-  <div class="box">
-    <a href="#">
-      <img src="<?= get_bloginfo('template_url') ?>/imgs/img-box.jpg" class="float-left" />
-      <h4 class="fonte-textos">san sapien, nec pulvinar mauris vestibulum blandit. Nullam congue felis in tellus vulputate 
-        id lobortis.</h4>
-      <div class="saiba-mais fonte-textos">Saiba +</div>
-    </a>
-  </div>    
+  <? } ?>
+
 
   <div class="clear"></div>    
 
