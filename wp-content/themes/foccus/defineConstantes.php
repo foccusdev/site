bec33fe029528d881b97a0adb8ef74d5761ae952
João <joaogabrielv@gmail.com>
2013-06-26 22:18:44 -0300

    definindo constantes para conceito

--- wp-content/themes/foccus/defineConstantes.php
+++ wp-content/themes/foccus/defineConstantes.php
@@ -1,4 +1,5 @@
 <?php
+
 //Magno correção de zica
 // Cria constantes para as seções do site
 define('_NOTICIAS', 2);
@@ -8,16 +9,15 @@ define('_EQUIPE', 5);
 define('_AMBIENTE', 6);
 define('_DEPOIMENTOS', 10);
 define('_PARCEIROS', 11);
-
+define('_CONCEITO_TEXTO_CIMA', 54);
+define('_CONCEITO_TEXTO_BAIXO', 51);
 
 define('_CONTATO', 45);
 
 
 if ($_SERVER['HTTP_HOST'] == 'localhost') {
   define('_EMAILS', 107);
-  define('_CONCEITO_TEXTO_CIMA', 54);
-  define('_CONCEITO_TEXTO_BAIXO', 51);
-}else{
+} else {
   define('_EMAILS', 114);
 }
 ?>
