1d3429c243b6272f856943d3d857de77320096f0
João <joaogabrielv@gmail.com>
2013-07-13 21:24:45 -0300

    Formulários de newsletter responsivos

--- wp-content/themes/foccus/conceito.php
+++ wp-content/themes/foccus/conceito.php
@@ -27,6 +27,12 @@ get_template_part('incs/topo');
   <h2 class="nome-secao">Conceito</h2>
   <span class="linha-azul"></span>
 
+  <?
+  $wp_query = new WP_Query('p=' . _CONCEITO_TEXTO_BAIXO);
+  the_post();
+  $mensagemIncentivo = get_the_excerpt();
+  ?>  
+
   <div class="conteudo-interno fonte-textos float-left">
     <?
     $wp_query = new WP_Query('p=' . _CONCEITO_TEXTO_CIMA);
@@ -34,6 +40,16 @@ get_template_part('incs/topo');
     if (has_post_thumbnail())
       the_post_thumbnail('post-thumbnail', array('class' => 'img-conceito float-left'));
     ?>  
+
+    <div class="mensagem_incentivo" >
+      <div class="texto_img-circulo" >
+        <?=$mensagemIncentivo ?>
+      </div> 
+      <div class="img-circulo" >
+        <img src="<?= get_bloginfo('template_url') ?>/imgs/circulo_pontilhado.png"/>
+      </div> 
+    </div> 
+
 
     <div class="texto-conceito">
       <? the_content() ?>
@@ -45,15 +61,6 @@ get_template_part('incs/topo');
     ?>
 
     <div class="conceito-texto-baixo float-left">
-
-      <div class="mensagem_incentivo" >
-        <div class="texto_img-circulo" >
-          <p><b><i>Mensagem de incentivo</i></b></p>
-        </div> 
-        <div class="img-circulo" >
-          <img src="<?= get_bloginfo('template_url') ?>/imgs/circulo_pontilhado.png"/>
-        </div> 
-      </div> 
 
       <div class="objetivos">
         <div class="texto_objetivos"><? the_title() ?><img src="<?= get_bloginfo('template_url') ?>/imgs/img_objetivos.png"/></div>
